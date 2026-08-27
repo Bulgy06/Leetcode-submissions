4d4f8d63d15d863b891d2707ec9bdc260f888efd
Nikhil RVS <nikhil.rvs7@gmail.com>
2026-08-27 15:11:15 +0530

    Add: integer-break - submission-1

---
+++ Data Structures & Algorithms/integer-break/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    vector<int> dp;
+public:
+    int integerBreak(int n) {
+        dp.resize(n+1,0);
+        if(n<=3) return n-1;
+        dp[1]=0;
+        dp[2]=1;
+        dp[3]=2;
+        for(int i=4;i<=n;i++){
+            for(int j=1;j<=i/2;j++){
+                dp[i]=max(dp[i],j*(i-j));
+                dp[i]=max(j*dp[i-j],dp[i]);
+            }
+        }
+        return dp[n];
+    }
+};
